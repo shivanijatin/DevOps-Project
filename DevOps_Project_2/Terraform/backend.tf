41f4d55e377ead8af223b2abdac86cadd08a9ad4
shivanijatin <164661043+shivanijatin@users.noreply.github.com>
2024-06-16 11:19:22 +0530

    Update backend.tf
    s3 bucket updated name

--- DevOps_Project_2/Terraform/backend.tf
+++ DevOps_Project_2/Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "euran-terraform-statefile"
+    bucket = "euran-terraform-statefile-01"
     key = "server_name/statefile"
     region = "ap-south-1"
   }
